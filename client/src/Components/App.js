d6f51e7b40f963bb1a76a23df8b3c6a13d3ecc46
maximecharpentier <maximecharpentier0@gmail.com>
2020-04-25 16:06:06 +0200

    Add: Color variables, title and text global style

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-import Header from './organisms/Header';
+import Header from './organisms/Header/Header';
 import Footer from './organisms/Footer';
 
 import Home from './pages/Home';
