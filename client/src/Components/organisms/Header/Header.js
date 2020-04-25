7e95d369bc6b2e4a261360af5592e8ee1a57fa02
maximecharpentier <maximecharpentier0@gmail.com>
2020-04-25 16:10:58 +0200

    Remove: Header modifications

--- client/src/Components/organisms/Header/Header.js
+++ client/src/Components/organisms/Header/Header.js
@@ -2,24 +2,9 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header>
-            <h1 className="Header__brand">Gille Epie</h1>
-            <ul className="Header__items">
-                <li className="Header__item">
-                    <a className="Header__link" href="/">Facebook</a>
-                </li>
-                <li className="Header__item">
-                    <a className="Header__link" href="/">Twitter</a>
-                </li>
-                <li className="Header__item">
-                    <a className="Header__link" href="/">Youtube</a>
-                </li>
-                <li className="Header__item">
-                    <a className="Header__link" href="/">Instagram</a>
-                </li>
-            </ul>
-            <button className="Header__burger"></button>
-        </header>
+        <div>
+            <h1>Header</h1>
+        </div>
     )
 }
 
