4737f71f8fd9deec429bf468f6704493f1c0a082
ninolam <lamoureux.nino@gmail.com>
2020-04-26 19:31:27 +0200

    Add: install story book

---
+++ client/storybook-static/precache-manifest.9b3bbf57a4afad61c2635fac20c89359.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab50db73cf7454490a6d69c85b260fbd",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d59ebd7d57d1649bf2e2.bundle.js"
+  },
+  {
+    "url": "runtime~main.d59ebd7d57d1649bf2e2.bundle.js"
+  },
+  {
+    "url": "vendors~main.d59ebd7d57d1649bf2e2.bundle.js"
+  },
+  {
+    "url": "vendors~main.d59ebd7d57d1649bf2e2.bundle.js.LICENSE.txt"
+  }
+]);
