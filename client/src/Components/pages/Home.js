3d533da4f639876776e6af35efa3a7ec26df27e6
maximecharpentier <maximecharpentier0@gmail.com>
2020-04-26 19:46:24 +0200

    Add: Biography Component style & structure

--- client/src/Components/pages/Home.js
+++ client/src/Components/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Heading from './../organisms/Heading/Heading'
 import Restaurants from './../organisms/Restaurants/Restaurants'
 import Press from './../organisms/Press/Press'
+import Biography from './../organisms/Biography/Biography'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Heading />
       <Restaurants />
       <Press />
+      <Biography />
     </div>
   )
 }
