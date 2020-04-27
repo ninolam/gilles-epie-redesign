60dc16d357b6cdf32eefb32becb1bfa99f302f91
maximecharpentier <maximecharpentier0@gmail.com>
2020-04-27 14:15:03 +0200

    Add: Header dynamic content

--- client/src/Components/organisms/Header/Header.js
+++ client/src/Components/organisms/Header/Header.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import { Link } from "react-router-dom";
 
 const Header = () => {
+
+  const [ brand ] = useState('Gille Epie')
+
+  const [socialItems, setSocialItems] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:27017/api/socials")
+      .then(res => {
+        setSocialItems(res.data)
+        console.log(res.data)
+      })
+  }, []);
+
   return (
     <header className="Header">
       <div className="Header__wrapper content-wrapper">
         <div className="Header__container">
-          <h1 className="Header__brand">Gille Epie</h1>
+          <Link to="/">
+            <h1 className="Header__brand">{brand}</h1>
+          </Link>
           <ul className="Header__items">
-            <li className="Header__item">
-              <a className="Header__link" href="/">Facebook</a>
-            </li>
-            <li className="Header__item">
-              <a className="Header__link" href="/">Twitter</a>
-            </li>
-            <li className="Header__item">
-              <a className="Header__link" href="/">Youtube</a>
-            </li>
-            <li className="Header__item">
-              <a className="Header__link" href="/">Instagram</a>
-            </li>
+            {socialItems.map((socialItem, id) =>
+              <li className="Header__item" key={id}>
+                <a className="Header__link" href={socialItem.url} target="_blank">{socialItem.name}</a>
+              </li>
+            )}
           </ul>
         </div>
         <button className="Header__burger"></button>
