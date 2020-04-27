fb7dbd2c68cd3e57b88d0984d991ceb0adfa3fcc
Nancy_Cpbl <nancy.campbell@hetic.net>
2020-04-27 21:58:42 +0200

    Update: buttons atom

--- client/src/stories/Buttons.stories.js
+++ client/src/stories/Buttons.stories.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Button from '../Components/atoms/buttons/Buttons';
-import Icon from '../assets/img/arrow.svg';
+import ArrowIcon from '../assets/img/arrow.svg';
+import WhiteArrowIcon from '../assets/img/white-arrow.svg';
+import '../Components/atoms/buttons/Buttons.scss';
 
 export default {
   title: 'Buttons'
 }
 
 export const Red = props => <Button type='redButton' testid="redButton-button" onClick={props.onClick}><span>Button</span></Button>;
+export const RedIcon = props => <Button type='redButton' testid="redIconButton-button" onClick={props.onClick}><span className="arrow">Button<img className="arrow-icon" src={ArrowIcon}/></span></Button>;
 export const Black = props => <Button type='blackButton' testid="blackButton-button" onClick={props.onClick}><span>Button</span></Button>;
 export const Disabled = props => <Button type='redButton' disabled testid="redButton-button" onClick={props.onClick}><span>Button</span></Button>;
-
-//export const RedIcon = props => <Button type='redButton' icon testid="iconRedButton-button" onClick={props.onClick}><span><img src={Icon}/>Button</span></Button>;
+export const RedIconDisabled = props => <Button type='redButton' disabled testid="redIconDisabledButton-button" onClick={props.onClick}><span className="arrow">Button<img className="arrow-icon" src={ArrowIcon}/></span></Button>;
+export const Hover = props => <Button type='blackButton' testid="blackIconButton-button" onClick={props.onClick}><span className="arrow white"><img className="arrow-icon white" src={WhiteArrowIcon}/></span></Button>;
