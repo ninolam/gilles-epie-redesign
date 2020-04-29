65629ada454802e8dc998712784644efa575dc56
blyndusk <alexandre.delaloy.dev@gmail.com>
2020-04-29 18:08:56 +0200

    Update: konami code

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,25 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 import Footer from "./organisms/Footer";
 import Header from "./organisms/Header/Header";
+import Menu from "./organisms/Menu/Menu";
+import Konami from "./organisms/Konami/Konami";
 
 import Home from "./pages/Home";
 import PageList from "./pages/PageList/PageList";
 import PageDetails from "./pages/PageDetails/PageDetails";
+import Restaurant from "./pages/Restaurant/Restaurant";
 
 import konami from "../utils/konami.util";
 
 const App = () => {
+  const [isKonamiCodeFounded, setIsKonamiCodeFounded] = useState(false);
   useEffect(() => {
-    konami(() => alert("KONAMICODE"));
+    konami(() => setIsKonamiCodeFounded(true));
   });
   return (
     <div className="App">
+      {isKonamiCodeFounded ? (
+        <Konami setIsKonamiCodeFounded={setIsKonamiCodeFounded} />
+      ) : null}
       <Router>
         <Header />
         <Switch>
           <Home exact path="/" />
+          <Restaurant path="/restaurants/:id" title="Restaurant" />
           <PageList exact path="/recipes" title="Recettes" />
           <PageList exact path="/articles" title="Press" />
           <PageDetails path="/:path/:id" />
